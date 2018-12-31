181a41adfd3426351c69278ebc57f66c926059d2
AnitaI <anita.irsevic@gmail.com>
2018-12-31 16:34:37 +1300

    add keyword cipher and comments

--- vigenere.c
+++ vigenere.c
@@ -1,47 +1,38 @@
-// Encrypts by integer value - shifts input text by integer arg value e.g. a + 2 = c
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_TXT_SIZE 256
 
 int main(int argc, const char *argv[])
 {
     if (argc < 2)
     {
-        printf("ERROR: Missing key arg. Please enter int as argument to script call.\n");
+        printf("ERROR: Missing key arg. Please enter string as argument to script call.\n");
     }
     else
     {
-        int key = atoi(argv[1]);
-
-        // Conversion error handling
-        
-        if (key == 0)
+        for (int x = 0; x < strlen(argv[1]); x++)
         {
-            printf("Invalid argument. You entered '%s', please enter integer for encryption key.\n", argv[1]);
+            if (isalpha(argv[1][x]) == 0)
+            {
+                printf("ERROR: Argument needs to be all alpha letters.\n");
+                exit(1);
+            }
         }
 
         // Get string to encrypt
 
-        char freetext[256];
+        char *freetext = malloc(MAX_TXT_SIZE);
         int invalid_input;
 
         do
         {
             invalid_input = 0;
             printf("Please enter freetext to encrypt: ");
-            int result = scanf("%s", freetext);
-            if (result == EOF)
+            if (fgets(freetext, MAX_TXT_SIZE, stdin) == NULL)
             {
-                printf("Invalid input. Please enter string value.\n");
-                invalid_input = 1;
-            }
-            else if (result == 0)
-            {
-            while (fgetc(stdin) != '\n') // Read until a newline is found
-                ;
                 printf("Invalid input. Please enter string value.\n");
                 invalid_input = 1;
             }
@@ -53,21 +44,29 @@ int main(int argc, const char *argv[])
         while(invalid_input > 0);
 
         char encrypted_text[256];
-        for (int i = 0; i < strlen(freetext); i++)
+        int x = strlen(argv[1]);
+
+        for (int i = 0, y = 0; i < strlen(freetext); i++)
         {
             char cur_char = freetext[i];
             if (isalpha(cur_char))
             {
-                int cur_alpha_index = (tolower(cur_char) - 'a');
-                int encrypt_alpha_index = (cur_alpha_index + key) % 26;
+                int cur_txt_alpha_index = (tolower(cur_char) - 'a');
+                int index_key = y % x;
+                char key = argv[1][index_key];
+                int cur_key_index = (tolower(key) - 'a');
+                printf("%c\n",key);
+                int encrypt_alpha_index = (cur_txt_alpha_index + cur_key_index) % 26;
                 encrypted_text[i] = encrypt_alpha_index + 97;
                 if (isupper(cur_char))
                 {
                    encrypted_text[i] = toupper(encrypted_text[i]);
                 }
+                y++;
             }
             else
             {
+                printf("\n");
                 encrypted_text[i] = cur_char;
             }
         }
